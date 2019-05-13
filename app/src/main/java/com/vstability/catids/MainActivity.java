205b3575196eda5955ad3b7248b25a7f9ccab163
Valeriu Olariu <ovaleriu7@gmail.com>
2019-05-13 14:32:47 +0100

    Adding App adds Admob + TensFlow

--- app/src/main/java/com/vstability/catids/MainActivity.java
+++ app/src/main/java/com/vstability/catids/MainActivity.java
@@ -5,13 +5,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private AdView mAdView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // andorid Admob code
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
 
         Button knowlage = (Button)findViewById(R.id.yknowlage);
         Button camera1 = (Button)findViewById(R.id.camera);
